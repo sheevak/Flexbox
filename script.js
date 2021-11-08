13f853e858f0435873886d521fc1a81fff48c3c5
Sheeva Kavousian <sheevakavous@gmail.com>
2021-11-08 23:33:26 +0000

    Updated to select tags & added item functionality

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function property(elementId, value, property) {
-    document.getElementById(elementId).style.setProperty(property, value);
+function property(elementId, select, property) {
+    document.getElementById(elementId).style.setProperty(property, select.value);
 };
 
-function Dimentions(width, height) {
+function dimentions(width, height) {
     document.getElementById("cont").style.setProperty("width", width+"px");
     document.getElementById("cont").style.setProperty("height", height+"px");
 };
+
+function itemSelect(select) {
+    selectedItem = select.value;
+};
+
+let selectedItem = 1;
